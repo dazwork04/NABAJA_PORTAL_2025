777f59abcd101170d1be3b939161fe335c9e02d5
dazwork04 <daniellelorezco@gmail.com>
2025-05-16 15:02:34 +0800

    Fix typo in option value for Nabaja Land Corporation in vw_load-db.php

--- proc/views/vw_load-db.php
+++ proc/views/vw_load-db.php
@@ -1,4 +1,4 @@
 <?php
-echo '<option value="NCL-SVR">Nabaja Land Corporation</option>';
+echo '<option value="NLC-SVR">Nabaja Land Corporation</option>';
 // echo '<option value="TEST_NLC_LIVE">TEST</option>';
 ?>
