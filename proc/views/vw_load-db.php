63545e1ea4e0d3257f78ad5227f3e91dc49b4fc8
dazwork04 <daniellelorezco@gmail.com>
2025-05-22 09:26:33 +0800

    Fix option value format for Nabaja Land Corporation in vw_load-db.php

--- proc/views/vw_load-db.php
+++ proc/views/vw_load-db.php
@@ -1,4 +1,4 @@
 <?php
-echo '<option value="NLC-SVR">Nabaja Land Corporation</option>';
+echo '<option value="NLC_SVR">Nabaja Land Corporation</option>';
 // echo '<option value="TEST_NLC_LIVE">TEST</option>';
 ?>
